b34671762a190897f55b285cf7edc311a0746d7f
oringjea@gmail.com <marialuisatinaja@gmail.com>
2023-11-21 20:46:55 +0800

    commit

--- App.js
+++ App.js
@@ -1,10 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Modal, Alert } from 'react-native';
+import { ScrollView, TouchableOpacity } from 'react-native-web';
+import guest from './src/Guest';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  const show = (guestss) =>{
+    setModalVisible(true);
+    setSelectedUser(guestss);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+                {selectedUser &&(
+                  <Text style={{fontSize: 28, fontWeight: '500', padding: 15}}>Hello! {selectedUser.nick}</Text>
+                )}
+                {selectedUser && (
+                  <Text style={{fontSize: 28, fontWeight: '100'}}>{selectedUser.name.fname} {selectedUser.name.lname}.
+                  </Text>
+                )}
+                {selectedUser && (
+                  <Text style={{fontWeight: '100', fontSize: 22}}>{selectedUser.course}</Text>
+                )}
+
+                <TouchableOpacity onPress={()=> setModalVisible(false)}>
+                  <Text style={{marginTop: 25, fontSize: 20, color: '#776B5D'}}>Hide</Text>
+                </TouchableOpacity>
+
+            </View>
+          </View>
+        </Modal>
+
+      <Text style={styles.txt}>SCROLLVIEW</Text>
+      {guest.map ((guestss)=>{
+        return(
+          <View>
+            <ScrollView>
+              <TouchableOpacity onPress={()=> show(guestss)} style={styles.border}>
+                <Text style={styles.txt1}>{guestss.nick}</Text>
+              </TouchableOpacity>
+
+            </ScrollView>
+
+          </View>
+        )
+      })}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +67,45 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    marginTop: 10,
+  },
+  txt: {
+    fontWeight: 'bold',
+    fontSize: 35,
+    color: '#FF6C22',
+    marginLeft: 10
+
+  },
+  txt1: {
+    fontWeight: '500',
+    fontSize: 25,
+    marginLeft: 15,
+    margin: 10
+  },
+  border: {
+    borderWidth: 1,
+    borderColor: '#A7D397'
+
+  },
+  centeredView: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.50,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
---
+++ src/Guest.js
@@ -0,0 +1,131 @@
+const DATA = [
+    {
+      id: 0,
+      name: { 
+        fname: "MARIA LUISA",
+        lname: "TINAJA"
+      },
+      nick: "LUI",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 1,
+      name: { 
+        fname: "JEA",
+        lname: "ORING"
+      },
+      nick: "YANG",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 2,
+      name: { 
+        fname: "ANNA",
+        lname: "ANIB"
+      },
+      nick: "ANN",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 3,
+      name: { 
+        fname: "GELO",
+        lname: "HIBAYA"
+      },
+      nick: "GEL",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 4,
+      name: { 
+        fname: "MELROSE",
+        lname: "VALIENTE"
+      },
+      nick: "MEL",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 5,
+      name: { 
+        fname: "TASH",
+        lname: "RAN"
+      },
+      nick: "ASH",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 6,
+      name: { 
+        fname: "JOHNMARK",
+        lname: "LAGRIMAS"
+      },
+      nick: "MARK",
+      course: "BSIT - 3B"
+  },
+  {
+      id: 7,
+      name: { 
+        fname: "JESS",
+        lname: "TONG"
+      },
+      nick: "JES",
+      course: "BSIT - 3B"
+  },
+  {
+    id: 8,
+    name: { 
+      fname: "LUI",
+      lname: "LIM"
+    },
+    nick: "LUI",
+    course: "BSIT - 3A"
+  },
+  {
+    id: 9,
+    name: { 
+      fname: "ALBERT",
+      lname: "ANIB"
+    },
+    nick: "BERT",
+    course: "BSIT - 3A"
+  },
+  {
+    id: 10,
+    name: { 
+      fname: "PEARL",
+      lname: "TAN"
+    },
+    nick: "PEARL",
+    course: "BSIT - 3A"
+  },
+  {
+    id: 11,
+    name: { 
+      fname: "JILLIAN",
+      lname: "HAN"
+    },
+    nick: "JILL",
+    course: "BSIT - 3A"
+  },
+  {
+    id: 10,
+    name: { 
+      fname: "DIANE",
+      lname: "LIM"
+    },
+    nick: "DIANNY",
+    course: "BSIT 4"
+  },
+  {
+    id: 11,
+    name: { 
+      fname: "NATHANIEL",
+      lname: "UY"
+    },
+    nick: "NATH",
+    course: "BSCOMSCI 3"
+  },
+
+  ];
+
+  export default DATA;
